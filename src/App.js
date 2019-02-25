c934b7fffe106ef83715bb71b91f61c83fe452c9
mkcnly <mkcnly@gmail.com>
2019-02-25 13:31:57 -0700

    base app

--- src/App.js
+++ src/App.js
@@ -7,19 +7,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Current Mode:
         </header>
+        <div className="main">
+          Light Mode <label className="switch">
+            <input type="checkbox"  />
+            <span className="slider round"></span>
+          </label> Dark Mode
+        </div>
       </div>
     );
   }
